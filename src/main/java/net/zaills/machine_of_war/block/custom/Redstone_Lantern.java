6c9a15805da6bbdfd2861a40c69e58d81301f8f9
Zaills <39807152+Zaills@users.noreply.github.com>
2022-06-10 15:43:21 +0200

    the lamp is here but still no texture tho

--- src/main/java/net/zaills/machine_of_war/block/custom/Redstone_Lantern.java
+++ src/main/java/net/zaills/machine_of_war/block/custom/Redstone_Lantern.java
@@ -1,22 +1,74 @@
 package net.zaills.machine_of_war.block.custom;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.LanternBlock;
-import net.minecraft.particle.DustParticleEffect;
+import net.minecraft.block.*;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.state.property.Property;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
-import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
+import org.jetbrains.annotations.Nullable;
 
-public class Redstone_Lantern extends LanternBlock {
+public class Redstone_Lantern extends RedstoneTorchBlock implements Waterloggable{
+    public static final BooleanProperty HANGING;
+    public static final BooleanProperty WATERLOGGED;
+    protected static final VoxelShape STANDING_SHAPE;
+    protected static final VoxelShape HANGING_SHAPE;
 
     public Redstone_Lantern(AbstractBlock.Settings settings) {
         super(settings);
+        this.setDefaultState((BlockState)((BlockState)((BlockState)this.stateManager.getDefaultState()).with(HANGING, false)).with(WATERLOGGED, false));
     }
+
+    @Nullable
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        FluidState fluidState = ctx.getWorld().getFluidState(ctx.getBlockPos());
+        Direction[] var3 = ctx.getPlacementDirections();
+        int var4 = var3.length;
+
+        for(int var5 = 0; var5 < var4; ++var5) {
+            Direction direction = var3[var5];
+            if (direction.getAxis() == Direction.Axis.Y) {
+                BlockState blockState = (BlockState)this.getDefaultState().with(HANGING, direction == Direction.UP);
+                if (blockState.canPlaceAt(ctx.getWorld(), ctx.getBlockPos())) {
+                    return (BlockState)blockState.with(WATERLOGGED, fluidState.getFluid() == Fluids.WATER);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return (Boolean)state.get(HANGING) ? HANGING_SHAPE : STANDING_SHAPE;
+    }
+
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(new Property[]{HANGING, WATERLOGGED});
+    }
+
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        Direction direction = attachedDirection(state).getOpposite();
+        return Block.sideCoversSmallSquare(world, pos.offset(direction), direction.getOpposite());
+    }
+
+    protected static Direction attachedDirection(BlockState state) {
+        return (Boolean)state.get(HANGING) ? Direction.DOWN : Direction.UP;
+    }
+
+    static {
+        HANGING = Properties.HANGING;
+        WATERLOGGED = Properties.WATERLOGGED;
+        STANDING_SHAPE = VoxelShapes.union(Block.createCuboidShape(5.0, 0.0, 5.0, 11.0, 7.0, 11.0), Block.createCuboidShape(6.0, 7.0, 6.0, 10.0, 9.0, 10.0));
+        HANGING_SHAPE = VoxelShapes.union(Block.createCuboidShape(5.0, 1.0, 5.0, 11.0, 8.0, 11.0), Block.createCuboidShape(6.0, 8.0, 6.0, 10.0, 10.0, 10.0));
+}
 
 
 }
